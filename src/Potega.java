77c9c6416e6bf12a01de10e5adf150b8fd999bc4
przemjan <przemjanisz2@gmail.com>
2022-10-12 09:43:16 +0200

    commit nr 5

---
+++ src/Potega.java
@@ -0,0 +1,14 @@
+public class Potega {
+    public static void main(String[] args) {
+
+        int liczba1 =2, liczba2 = 7;
+
+        double wynik;
+
+        // liczba1 do potęgi liczba2
+        wynik = Math.pow(liczba1,liczba2);
+
+
+    }
+
+}
